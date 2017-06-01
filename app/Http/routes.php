de44ed1f2b04efa635cfb1dfc146a57331ccc40e
user <Global@mail.ua>
2017-06-01 20:15:04 +0300

    change routs, add about and contacts, change style css, download some pictures (right)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,11 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('/about', function(){
+    return view('about');
+});
+
+Route::get('/contacts', function(){
+    return view('contacts');
+});
---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('content')
+<canvas id="pixie"></canvas>
+<div>
+    <h2>Contacts</h2>
+    <p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+</div>
+<div id="left">
+    <p>City: Dnipro,</p>
+    <p>Red street, 21G</p>
+    <p>We are there!</p>
+    <img src="/images/right.png" alt="right">
+</div>
+<div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1573.2541049784174!2d35.04058107482225!3d48.45868852182342!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1496336346646" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+@endsection
