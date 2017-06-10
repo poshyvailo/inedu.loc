7022b0418963c62a794aee2f6c32e34038901da4
vkorniyenko <korney2011@gmail.com>
2017-06-10 15:11:56 +0300

    add contact form

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,19 +1,30 @@
 @extends('layouts.app')
 @section('content')
-<canvas id="pixie"></canvas>
-<div>
-    <h2>Contacts</h2>
-    <p>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
-</div>
-<div id="left">
-    <p>City: Dnipro,</p>
-    <p>Red street, 21G</p>
-    <p>We are there!</p>
-    <img src="/images/right.png" alt="right">
-</div>
-<div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1573.2541049784174!2d35.04058107482225!3d48.45868852182342!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1496336346646" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+<div id="contact" class="container-fluid bg-grey">
+  <h2 class="text-center">КОНТАКТЫ</h2>
+  <div class="row">
+    <div class="col-sm-5">
+      <p>Напишите нам и Мы ответим Вам в течении 24 часов.</p>
+      <p><span class="glyphicon glyphicon-map-marker"></span> Украина, Днепр</p>
+      <p><span class="glyphicon glyphicon-phone"></span> +38(012)345-67-89</p>
+      <p><span class="glyphicon glyphicon-envelope"></span> myemail@something.com</p>
+    </div>
+    <div class="col-sm-7 slideanim">
+      <div class="row">
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="name" name="name" placeholder="Имя" type="text" required>
+        </div>
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+        </div>
+      </div>
+      <textarea class="form-control" id="comments" name="comments" placeholder="Ваш комментарий" rows="5"></textarea><br>
+      <div class="row">
+        <div class="col-sm-12 form-group">
+          <button class="btn btn-default pull-right" type="submit">Отправить</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
