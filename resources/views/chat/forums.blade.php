0faf2f29d909fd408a1ea5e15517d2985c3abe52
vkorniyenko <korney2011@gmail.com>
2017-06-13 22:27:52 +0300

    add database chats and migrate, modify style.css, add in app.blade js scripts, add in forums.blade markup

--- resources/views/chat/forums.blade.php
+++ resources/views/chat/forums.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.app')
-
 @section('content')
-<div>
-    
+<div id="msg-box">
+  <ul>
+  </ul>
 </div>
+<form id="t-box" action="?" style="">
+  Имя: <input type="text" class='name' style="width:100px;" >
+  <input type="text" class='msg' style="width:500px;" >
+  <input type="submit" value="Отправить" style="margin-top:5px;">
+</form>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,33 +1,35 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Home</title>
-    <script src="/js/jquery-3.2.1.min.js" type="text/javascript"></script>
-    <script src="/js/bootstrap.min.js" type="text/javascript"></script>
-    <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-    <link href="/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
-    <link href="/css/style.css" rel="stylesheet" type="text/css"/>
-</head>
-<body>
-@include('menu.main')
-<main>
-    @if (Auth::guest())
-        <div class="container">
-            @yield('content')
-        </div>
-    @else
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3">
-                    @include('menu.left-menu')
-                </div>
-                <div class="col-md-9">
-                    @yield('content')
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+        <script src="/js/jquery-3.2.1.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="js/jquery.timers.js"></script>
+        <script type="text/javascript" src="js/jquery.cookie.js"></script>
+        <script src="/js/bootstrap.min.js" type="text/javascript"></script>
+        <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+        <link href="/css/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        @include('menu.main')
+        <main>
+            @if (Auth::guest())
+            <div class="container">
+                @yield('content')
+            </div>
+            @else
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        @include('menu.left-menu')
+                    </div>
+                    <div class="col-md-9">
+                        @yield('content')
+                    </div>
                 </div>
             </div>
-        </div>
-    @endif
-</main>
-</body>
+            @endif
+        </main>
+    </body>
 </html>
