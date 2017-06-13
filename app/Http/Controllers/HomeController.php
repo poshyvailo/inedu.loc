8542d6ae90312e33efb3f6a84f52334d0504b14f
user <Global@mail.ua>
2017-06-13 20:11:21 +0300

    edit contacts.blade (move tag form), edit bug with email in HomeController, create new tamplate mail form for users.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller {
      * @return void
      */
     public function __construct() {
-        $this->middleware('auth');
+	$this->middleware('auth');
     }
 
     /**
@@ -23,34 +23,34 @@ class HomeController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function index() {
-        return view('home');
+	return view('home');
     }
 
     public function about() {
-         return view('about');
+	return view('about');
     }
-    
+
     public function contacts() {
-         return view('contacts');
+	return view('contacts');
     }
-    
-    public function send(Request $request){
-        $email=$request->email;
-        $name=$request->name;
-        $comments=$request->comments;
-        
-        Mail::send('mail.email', 
-        ['name'=>$name,
-         'comments'=>$comments],
-        
-            function ($message) use ($email, $name){
-            $message->from('inedu.notice@gmail.com', 'Laravel');
-            $message->name($name);
-            $message->to($email);
-            });
-            
-        $request->session()->flash('status', 'Сообещение отправлено успешно!');
-        return redirect('/email');
-        }
+
+    public function send(Request $request) {
+	$email = $request->email;
+	$name = $request->name;
+	$comments = $request->comments;
+
+
+	Mail::send('mail.email', ['name' => $name,
+	    'comments' => $comments], function ($message) use ($email, $name) {
+	    $message->from($email);
+	    $message->cc($email);
+	    $message->to('inedu.notice@gmail.com', 'Laravel');
+	    $message->subject('Вопрос от пользователя');
+	    $message->replyTo($email);
+	});
+
+	$request->session()->flash('status', 'Сообещение отправлено успешно!');
+	return redirect('/contacts');
+    }
 
 }
--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -9,25 +9,25 @@
             <p><span class="glyphicon glyphicon-phone"></span> +38(012)345-67-89</p>
             <p><span class="glyphicon glyphicon-envelope"></span> myemail@something.com</p>
         </div>
-        <div class="col-sm-7 slideanim">
-            <div class="row">
-                <div class="col-sm-6 form-group">
-                    <input class="form-control" id="name" name="name" placeholder="Имя" type="text" required>
-                </div>
-                <div class="col-sm-6 form-group">
-                    <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
-                </div>
-            </div>
-            <textarea class="form-control" id="comments" name="comments" placeholder="Ваш комментарий" rows="5"></textarea><br>
-            <div class="row">
-                <form class="form-horizontal" role="form" method="POST" action="{{ url('/contacts/') }}" enctype="multipart/form-data">
-                    {{ csrf_field() }}
+	<form role="form" method="POST" action="{{ url('/contacts/') }}" enctype="multipart/form-data">
+	    {{ csrf_field() }}
+	    <div class="col-sm-7 slideanim">
+		<div class="row">
+		    <div class="col-sm-6 form-group">
+			<input class="form-control" id="name" name="name" placeholder="Имя" type="text" required>
+		    </div>
+		    <div class="col-sm-6 form-group">
+			<input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+		    </div>
+		</div>
+		<textarea class="form-control" id="comments" name="comments" placeholder="Ваш комментарий" rows="5"></textarea><br>
+		<div class="row">
                     <div class="col-sm-12 form-group">
                         <button class="btn btn-default pull-right" type="submit">Отправить</button>
                     </div>
-                </form>
-            </div>
-        </div>
+		    </form>	
+		</div>
+	    </div>
     </div>
 </div>
 @endsection
