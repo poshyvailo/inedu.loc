1c91f94efab04ca209f6ecf17d15294f725378b2
dontTochMyNameFuckingBoy <maraffonskiy@gmail.com>
2017-06-15 20:49:38 +0300

    delete master.blade.php
    change @extends('layouts.app')

--- resources/views/messenger/index.blade.php
+++ resources/views/messenger/index.blade.php
@@ -1,7 +1,8 @@
-@extends('layouts.master')
+@extends('layouts.app')
 
 @section('content')
+<a href="/messages/create">Create New Message</a>
     @include('messenger.partials.flash')
-
+    
     @each('messenger.partials.thread', $threads, 'thread', 'messenger.partials.no-threads')
 @stop
